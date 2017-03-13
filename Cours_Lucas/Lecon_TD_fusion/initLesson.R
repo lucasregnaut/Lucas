a6ac16b28d7146adf7c66c1dd8b5ebf16d09253a
Lucas Regnaut <lucas.regnaut@gmail.com>
2017-03-13 15:52:01 +0100

    importation tables

--- Cours_Lucas/Lecon_TD_fusion/initLesson.R
+++ Cours_Lucas/Lecon_TD_fusion/initLesson.R
@@ -9,3 +9,14 @@
 }
 
 HTA2 <- read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "HTA2.csv"),sep = ";")
+HTA_MED <- read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "HTA_MED.csv"),sep = ";")
+
+TER1<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TERRAIN1.txt"),sep = ";")
+TER2<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TERRAIN2.txt"),sep = ";")
+TER3<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TERRAIN3.txt"),sep = ";")
+TER4<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TERRAIN4.txt"),sep = ";")
+
+TEL1<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TEL1.txt"),sep = ";")
+TEL2<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TEL2.txt"),sep = ";")
+TEL3<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TEL3.txt"),sep = ";")
+TEL4<-read.csv(file.path(.get_course_path(), "Cours_Lucas", "Lecon_TD_fusion", "TEL4.txt"),sep = ";")
